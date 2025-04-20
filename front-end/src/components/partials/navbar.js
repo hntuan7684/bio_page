2cf0f8bc6a382385d056f4b6bd92fcb13986832d
hntuan7684 <tuanhnt7684@gmail.com>
2025-04-20 18:46:00 +0700

    Navbar

--- front-end/src/components/partials/navbar.js
+++ front-end/src/components/partials/navbar.js
@@ -1,58 +1,50 @@
-import React, { useState } from "react"; // Để sử dụng useState
-import { Link } from 'react-router-dom';
-
+import React from "react";
 import "../../public/css/components/partials/navbar.css";
+
 import logo from "../../public/icon/logo.png";
 import language_icon from "../../public/icon/language_icon.png";
 function NavBar() {
-  const [language, setLanguage] = useState("EN"); // State để lưu trữ ngôn ngữ
-
-  const handleLanguageChange = (event) => {
-    setLanguage(event.target.value); // Cập nhật ngôn ngữ khi thay đổi
-  };
-
   return (
     <div className="navbar">
-      <div className="logo-container">
-        <Link href="/">
-            <img src={logo} width={100}></img>
-        </Link>
-      </div>
-      <nav >
-        <ul>
-          <li>
-            <Link href="/" aria-label="Go to home page">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/about" aria-label="Go to about page">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact" aria-label="Go to contact page">
-              Contact
-            </Link>
-          </li>
-          <li>
-          <Link href="/login" aria-label="Go to login page">
-              Log In
-            </Link>
-          </li>
-          <li className="register-container">
-            <Link href="/register" aria-label="Go to register page">
-              Register
-            </Link>
-          </li>
-          <img src={language_icon} alt="Language icon" width={22} />
-          <li className="language-container">
-            <select value={language} onChange={handleLanguageChange}>
-              <option value="EN">EN</option>
-              <option value="VN">VN</option>
+      <nav>
+        <div className="logo-container">
+          <a href="/">
+            <img src={logo} alt="Logo" width={100} />
+          </a>
+        </div>
+        <div></div>
+
+        <div className="tab-container">
+          <ul className="navbar-tab">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/services">Services</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+
+          <div className="register-container">
+            <div>Sign Up</div>
+          </div>
+
+          <div className="language-container">
+            <select>
+              <option value="en">EN</option>
+              <option value="vn">VN</option>
             </select>
-          </li>
-        </ul>
+            <img src={language_icon} alt="Language" />
+          </div>
+        </div>
       </nav>
     </div>
   );
